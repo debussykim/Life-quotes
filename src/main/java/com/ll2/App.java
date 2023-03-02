9222a30213c7cf61025632b787fc189668c3168d
debussysuh <debussysuh@gmail.com>
2023-03-02 13:52:56 +0900

    명령어 파싱

--- src/main/java/com/ll2/App.java
+++ src/main/java/com/ll2/App.java
@@ -4,9 +4,7 @@ import com.ll2.system.controller.SystemController;
 import com.ll2.wiseSaying.controller.WiseSayingController;
 import com.ll2.wiseSaying.entity.WiseSaying;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
 
@@ -26,6 +24,27 @@ public class App {
             }
             else if(input.equals("목록")){
                 wiseSayingController.list();
+            }
+            else if(input.startsWith("삭제")){
+                // 정리 시작
+                String[] inputBits = input.split("\\?", 2); //최대 2개까지 ?를 기준으로 나누기
+                //System.out.println(Arrays.toString(inputBits));
+                String actionCode = inputBits[0];
+
+                Map<String, String> params = new HashMap<>();
+                String[] paramBits = inputBits[1].split("&");
+                for (String paramStr : paramBits) {
+                    String[] paramStrBits = paramStr.split("=", 2);
+                    String key = paramStrBits[0];
+                    String value = paramStrBits[1];
+                    params.put(key, value);
+                }
+
+                // 정리 끝
+                System.out.printf("actionCode: %s\n", actionCode);
+                System.out.printf("params: %s\n", params);
+
+                wiseSayingController.remove();
             }
             else if(input.equals("종료")){
                 systemController.exit();
