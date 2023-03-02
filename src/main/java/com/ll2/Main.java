a28a5a4ceef8965b6ab55aaeaf095c73ce34297a
debussysuh <debussysuh@gmail.com>
2023-03-02 09:38:11 +0900

    3단계- 등록시 생성된 명언번호 노출

--- src/main/java/com/ll2/Main.java
+++ src/main/java/com/ll2/Main.java
@@ -10,12 +10,16 @@ public class Main {
         System.out.printf("명령) ");
         String input = sc.nextLine();
 
+        int n = 0;
+
         while(true) {
             if (input.equals("등록")){
+                n++;
                 System.out.printf("명언 : ");
                 String quotes = sc.nextLine();
                 System.out.printf("작가 : ");
                 String author = sc.nextLine();
+                System.out.println(n+"번 명언이 등록되었습니다.");
                 System.out.printf("명령) ");
                 input = sc.nextLine();
             }
