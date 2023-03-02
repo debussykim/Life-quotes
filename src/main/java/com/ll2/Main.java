97791b3fc5104c1ec737a6d570fd5e2076f6ed08
debussysuh <debussysuh@gmail.com>
2023-03-02 09:21:40 +0900

    1단계

--- src/main/java/com/ll2/Main.java
+++ src/main/java/com/ll2/Main.java
@@ -1,7 +1,15 @@
 package com.ll2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 SSG ==");
+        System.out.printf("명언) ");
+        String input = sc.nextLine();
+
+        sc.close();
     }
 }
