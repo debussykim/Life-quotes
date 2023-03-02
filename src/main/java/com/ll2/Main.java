a880a1459409447061af18c91be9ff30033f8602
debussysuh <debussysuh@gmail.com>
2023-03-02 09:28:46 +0900

    2단계

--- src/main/java/com/ll2/Main.java
+++ src/main/java/com/ll2/Main.java
@@ -7,8 +7,22 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 SSG ==");
-        System.out.printf("명언) ");
+        System.out.printf("명령) ");
         String input = sc.nextLine();
+
+        while(true) {
+            if (input.equals("등록")){
+                System.out.printf("명언 : ");
+                String quotes = sc.nextLine();
+                System.out.printf("작가 : ");
+                String author = sc.nextLine();
+                System.out.printf("명령) ");
+                input = sc.nextLine();
+            }
+            else if(input.equals("종료")){
+                break;
+            }
+        }
 
         sc.close();
     }
